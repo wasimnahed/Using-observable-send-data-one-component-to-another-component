759e4c59610800b3246f26a66ae6e853660d074b
wasimnahed <Wasim.Nahed@softobiz.com>
2022-11-02 11:00:58 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[DataService]
+})
+export class AppComponent {
+  title = 'observable';
+
+constructor(private dataService:DataService){
+
+}
+
+}
